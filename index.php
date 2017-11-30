f2000234952d0f3e7610aec2f630c8112ffe7b59
Dark-Kun <logan.blangenois@live.com>
2017-11-30 20:46:16 +0100

    Ajout des balises principales HTML dans index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
-<nav>
-  <ul>
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-    <li>Item 4</li>
-    <li>Item 5</li>
-  </ul>
-</nav>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+      </ul>
+    </nav>
+  </body>
+</html>
